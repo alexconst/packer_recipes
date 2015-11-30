289075ec2829c4cba9fdc293c8f943b0a2db4e22
Alexandre Constantino <dreaming.about.electric.sheep@gmail.com>
2015-11-30 20:09:15 +0000

    Add QEMU builder for Debian 8 Jessie

---
+++ packer_variables.sh
@@ -0,0 +1,7 @@
+
+export PACKER_ISO_URL="$HOME/home/software_archive/linux_isos/debian/debian-8.2.0-amd64-netinst.iso"
+#export PACKER_ISO_URL="http://cdimage.debian.org/debian-cd/8.2.0/amd64/iso-cd/debian-8.2.0-amd64-netinst.iso"
+
+export PACKER_SSH_USER="vagrant"
+export PACKER_SSH_PASS="vagrant"
+
