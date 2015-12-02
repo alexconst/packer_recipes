e1b3d6639bd396782b2170650333fb6906ca33ee
Voight Kampff <dreaming.about.electric.sheep@gmail.com>
2015-12-02 21:06:30 +0000

    Add vbox and vmware builders. Update partition
    Add builders for virtualbox and vmware.
    Update the preseed file to use a single regular root partition.
    Tweak variables for the cd iso.

--- packer_variables.sh
+++ packer_variables.sh
@@ -1,7 +1,16 @@
+# Debian Jessie ISO (sha512 checksum )
+export PACKER_DEBIAN_ISO_URL="$HOME/home/software_archive/linux_isos/debian/debian-8.2.0-amd64-netinst.iso"
+#export PACKER_DEBIAN_ISO_URL="http://cdimage.debian.org/debian-cd/8.2.0/amd64/iso-cd/debian-8.2.0-amd64-netinst.iso"
+export PACKER_DEBIAN_ISO_SUM="923cd1bfbfa62d78aecaa92d919ee54a95c8fca834b427502847228cf06155e7243875f59279b0bf6bfd1b579cbe2f1bc80528a265dafddee9a9d2a197ef3806"
 
-export PACKER_ISO_URL="$HOME/home/software_archive/linux_isos/debian/debian-8.2.0-amd64-netinst.iso"
-#export PACKER_ISO_URL="http://cdimage.debian.org/debian-cd/8.2.0/amd64/iso-cd/debian-8.2.0-amd64-netinst.iso"
-
+# User to be created
 export PACKER_SSH_USER="vagrant"
 export PACKER_SSH_PASS="vagrant"
+
+# VirtualBox additions ISO (sha256 checksum)
+export PACKER_VBOX_ISO_URL="/usr/share/virtualbox/VBoxGuestAdditions.iso"
+export PACKER_VBOX_ISO_SUM="e5b425ec4f6a62523855c3cbd3975d17f962f27df093d403eab27c0e7f71464a"
+
+# AWS credentials
+# not declared here because they're sourced from AWS config files
 
---
+++ scripts/vmware.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -eux
+
+# https://github.com/vmware/open-vm-tools
+# https://github.com/vmware/open-vm-tools#will-the-commercial-version-vmware-tools-differ-from-the-open-source-version-open-vm-tools-if-so-how
+#   Will the commercial version (VMware Tools) differ from the open source version (open-vm-tools)? If so, how?
+#   However, we do currently make use of certain components licensed from third parties as well as components from other VMware products which are only available in binary form.
+# And advantage on using the open-vm-tools is that they are updated, while an older VMware version of the tools will not.
+use_open_vm_tools=1
+
+
+install_headers () {
+    apt-get install -y linux-headers-$(uname -r) build-essential perl
+    apt-get install -y dkms
+}
+
+clean_headers () {
+    apt-get -y remove linux-headers-$(uname -r) build-essential perl
+    apt-get -y autoremove
+}
+
+case "$PACKER_BUILDER_TYPE" in
+
+    "vmware-iso" | "vmware-ovf")
+        echo "Installing VMware Tools guest additions"
+        if [ $use_open_vm_tools -eq 0 ]; then
+            install_headers
+            iso_url="/var/tmp/linux.iso"
+            mkdir -p /mnt/cdrom
+            mount -o loop "$iso_url" /mnt/cdrom
+            mkdir -p /tmp/vmtools/ 
+            tar -zxvf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/vmtools 
+            /tmp/vmtools/vmware-tools-distrib/vmware-install.pl --default
+            umount /mnt/cdrom
+            rm "$iso_url"
+            rm -rf /tmp/vmtools
+            clean_headers
+        elif [ $use_open_vm_tools -eq 1 ]; then
+            install_headers
+            apt-get install -y open-vm-tools open-vm-tools-dkms
+            clean_headers
+        else
+            echo "ERROR: something went wrong. Incorrect choice of vm tools."
+            exit 3 
+        fi
+        ;;
+
+    *)
+        echo "Nothing to do here. This isn't a vmware builder type."
+        ;;
+
+esac
+
+
