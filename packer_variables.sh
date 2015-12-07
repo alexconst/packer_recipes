318c967a1dd5d6ffe5446120739816ee2c192e5a
Alexandre Constantino <dreaming.about.electric.sheep@gmail.com>
2015-12-07 02:06:18 +0000

    Add Vagrant post-processor for VirtualBox image

--- packer_variables.sh
+++ packer_variables.sh
@@ -1,4 +1,4 @@
-# Debian Jessie ISO (sha512 checksum )
+# Debian Jessie ISO (sha512 checksum)
 export PACKER_DEBIAN_ISO_URL="$HOME/home/software_archive/linux_isos/debian/debian-8.2.0-amd64-netinst.iso"
 #export PACKER_DEBIAN_ISO_URL="http://cdimage.debian.org/debian-cd/8.2.0/amd64/iso-cd/debian-8.2.0-amd64-netinst.iso"
 export PACKER_DEBIAN_ISO_SUM="923cd1bfbfa62d78aecaa92d919ee54a95c8fca834b427502847228cf06155e7243875f59279b0bf6bfd1b579cbe2f1bc80528a265dafddee9a9d2a197ef3806"
