5c4405f32dd83dc86a789fa95bd177247a57096e
Alex C <dreaming.about.electric.sheep@gmail.com>
2023-02-04 01:42:40 +0000

    cleanup

--- debian-11-bullseye-amd64/scripts/network.sh
+++ debian-11-bullseye-amd64/scripts/network.sh
@@ -15,43 +15,6 @@ done
 
 rm -f /etc/network/interfaces
 
-#sed -i -e '/^auto/d' /etc/network/interfaces
-#sed -i -e '/^iface/d' /etc/network/interfaces
-#sed -i -e '/^allow-hotplug/d' /etc/network/interfaces
-#sed -i 's/en[[:alnum:]]*/eth0/g' /etc/network/interfaces
-
-
-
-## Work around network device naming mismatch caused by Packer & Libvirt
-## TODO: figure out a better way to fix this
-#cat <<'OEOF' >> /etc/rc.local
-##!/bin/bash
-#
-##newif=$(dmesg | grep -i virtio_net | grep renamed | awk '{print $5}' | tr -d ':')
-#newif=$(ls -1 /sys/class/net/ | grep -v lo | head -n 1)
-#cat <<EOF > /etc/network/interfaces
-## This file is being generated at boot in order to workaround an issue related to Packer and Libvirt
-#
-#source /etc/network/interfaces.d/*
-#
-## The loopback network interface
-#auto lo
-#iface lo inet loopback
-#
-## The primary network interface
-##allow-hotplug $newif
-#auto $newif
-#iface $newif inet dhcp
-#pre-up sleep 2
-#EOF
-#OEOF
-#chmod +x /etc/rc.local
-
-## Ensures the network interface gets an IP (in theory this shouldn't be necessary but it actually fixes an issues I'm encountering)
-#apt-get install -y ifplugd
-#sed -i -e 's/INTERFACES=.*/INTERFACES="all"/g' /etc/default/ifplugd
-#systemctl enable ifplugd.service
-
 
 
 
